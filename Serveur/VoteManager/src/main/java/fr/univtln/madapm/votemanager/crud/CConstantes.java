522985a2fc1bc74ee520b10cac04157ed652c5ae
Sebi <sebi83140@gmail.com>
2015-05-13 15:00:58 +0200

    Ajout CRUD

---
+++ Serveur/VoteManager/src/main/java/fr/univtln/madapm/votemanager/crud/CConstantes.java
@@ -0,0 +1,15 @@
+package fr.univtln.madapm.votemanager.crud;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * Created by sebastien on 13/05/15.
+ */
+public class CConstantes {
+    public static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("mysqlconnection");
+    public static final EntityManager EM = EMF.createEntityManager();
+    public static final EntityTransaction ET = EM.getTransaction();
+}
