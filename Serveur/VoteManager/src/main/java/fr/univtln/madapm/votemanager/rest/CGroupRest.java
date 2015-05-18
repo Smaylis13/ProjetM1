08ab24a3bb0ff3a4bb348d92e2453d86c53dc7c9
Sebi <sebi83140@gmail.com>
2015-05-18 14:47:28 +0200

    Ajout REST

--- Serveur/VoteManager/src/main/java/fr/univtln/madapm/votemanager/rest/CGroupRest.java
+++ Serveur/VoteManager/src/main/java/fr/univtln/madapm/votemanager/rest/CGroupRest.java
@@ -1,13 +1,30 @@
 package fr.univtln.madapm.votemanager.rest;
 
-import javax.ws.rs.Path;
+import fr.univtln.madapm.votemanager.dao.CGroupDAO;
+import fr.univtln.madapm.votemanager.metier.user.CGroup;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by civars169 on 06/05/15.
  * copyright Christian
  */
 
-//@Path("groupe")
-public class CGroupeRest {
+@Path("/group")
+public class CGroupRest {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{pId}")
+    public CGroup read(@PathParam("pId") int pId){
+        CGroupDAO lGroupDAO = new CGroupDAO();
+        return lGroupDAO.findById(pId);
+    }
 
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public void addGroup(CGroup pGroup){
+        CGroupDAO lGroupDAO = new CGroupDAO();
+        lGroupDAO.create(pGroup);
+    }
 }
