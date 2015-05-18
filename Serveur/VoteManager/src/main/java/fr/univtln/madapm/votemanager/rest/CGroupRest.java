97f82ee8494d39cc67e8a8de78c21b5706fb2331
Kévin Fuster <kevinfuster83@gmail.com>
2015-05-18 14:44:06 +0200

    adds Group Rest

---
+++ Serveur/VoteManager/src/main/java/fr/univtln/madapm/votemanager/rest/CGroupRest.java
@@ -0,0 +1,13 @@
+package fr.univtln.madapm.votemanager.rest;
+
+import javax.ws.rs.Path;
+
+/**
+ * Created by civars169 on 06/05/15.
+ * copyright Christian
+ */
+
+//@Path("groupe")
+public class CGroupRest {
+
+}
