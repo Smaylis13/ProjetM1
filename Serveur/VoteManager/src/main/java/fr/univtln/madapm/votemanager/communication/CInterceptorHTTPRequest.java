36c553566fab41cbea5f386865d9f3d2429515d7
Kévin Fuster <kevinfuster83@gmail.com>
2015-05-20 12:25:17 +0200

    adds server Interceptor base

--- Serveur/VoteManager/src/main/java/fr/univtln/madapm/votemanager/communication/CInterceptorHTTPRequest.java
+++ Serveur/VoteManager/src/main/java/fr/univtln/madapm/votemanager/communication/CInterceptorHTTPRequest.java
@@ -1,15 +1,35 @@
 package fr.univtln.madapm.votemanager.communication;
 
+import org.eclipse.persistence.oxm.MediaType;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 /**
  * Created by civars169 on 20/05/15.
  */
 public class CInterceptorHTTPRequest implements ContainerRequestFilter {
     @Override
-    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
-        // TODO interceptor request
+    public void filter(ContainerRequestContext pContainerRequestContext) throws IOException {
+
+        InputStream lEntityStream = pContainerRequestContext.getEntityStream();
+        ByteArrayOutputStream lBaos = new ByteArrayOutputStream();
+        byte[] lBuffer = new byte[4096];
+        int lLength;
+        while ((lLength = lEntityStream.read(lBuffer,0,lBuffer.length)) >0) {
+            lBaos.write(lBuffer, 0, lLength);
+        }
+        lBaos.flush();
+        String lMsg=lBaos.toString();
+        //DECODER lMsg
+
+        pContainerRequestContext.setEntityStream(new ByteArrayInputStream(lMsg.getBytes()));
     }
 }
