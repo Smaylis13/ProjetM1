3cf75ba50c4b485c829c553367e606ef0fe68476
Christophe Hémon <christophe-hemon@etud.univ-tln.fr>
2015-05-20 16:11:02 +0200

    Début interface config uninomial 1 tour

--- Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CLoginActivity.java
+++ Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CLoginActivity.java
@@ -3,6 +3,7 @@ package fr.univtln.m1dapm.g3.g3vote.Interface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
--- Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CVoteConfUninominalOneTurn.java
+++ Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CVoteConfUninominalOneTurn.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import fr.univtln.m1dapm.g3.g3vote.Entite.CCandidat;
 import fr.univtln.m1dapm.g3.g3vote.R;
 
 /**
@@ -12,9 +16,28 @@ import fr.univtln.m1dapm.g3.g3vote.R;
  */
 public class CVoteConfUninominalOneTurn extends AppCompatActivity {
 
+    private ArrayList<CCandidat> listCandidat = new ArrayList<CCandidat>();
+    private CCandidat candidat = new CCandidat();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cvote_conf_uninominal_one_turn);
+
+        //Récupération du composant ListView
+        ListView list = (ListView)findViewById(R.id.lListViewMyVote);
+
+        //Récupération de la liste des personnes
+        ArrayList<CCandidat> listCandidat = new ArrayList<CCandidat>();
+        CCandidat candidat = new CCandidat();
+        listCandidat.add(candidat);
+
+        //Création et initialisation de l'Adapter pour les personnes
+        CCandidatAdapter adapter = new CCandidatAdapter(this, listCandidat);
+
+        //Initialisation de la liste avec les données
+        list.setAdapter(adapter);
+
     }
 
     @Override
@@ -22,5 +45,14 @@ public class CVoteConfUninominalOneTurn extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_cvote_conf_uninomial_one_turn, menu);
         return true;
+    }
+
+    public void addChoiceButton(View view) {
+
+
+
+    }
+
+    public void removeChoiceButton(View view) {
     }
 }
