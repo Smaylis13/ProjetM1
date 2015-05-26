828839858422850caf1a4ef8e1d05abce9e72968
Kévin Fuster <kevinfuster83@gmail.com>
2015-05-26 10:38:43 +0200

    adds CCommon

---
+++ Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Communication/CCommon.java
@@ -0,0 +1,16 @@
+package fr.univtln.m1dapm.g3.g3vote.Communication;
+
+/**
+ * Created by Ookami on 21/05/2015.
+ */
+public class CCommon {
+    public static String CLIENT_ID = "oauth2test";
+    public static String CLIENT_SECRET = "oauth2clientsecret";
+    public static String AUTHORIZATION_CODE = "oauth2authcode";
+    public static String USERNAME = "test";
+    public static String PASSWORD = "test";
+    public static String RESOURCE_SERVER_NAME = "resource";
+    public static final String ACCESS_TOKEN_VALID = "access_token_valid";
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+    public static final String AUTHORIZATION_HEADER_OAUTH2 = "Bearer " + ACCESS_TOKEN_VALID;
+}
