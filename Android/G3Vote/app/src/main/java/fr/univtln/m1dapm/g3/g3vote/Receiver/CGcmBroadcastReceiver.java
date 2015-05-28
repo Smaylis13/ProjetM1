f13a4dad769412d6867c6c7c27cb6a597465845f
ISMAIL GACHA <ismail-gacha@etud.univ-tln.fr>
2015-05-28 13:27:29 +0200

    maj

--- Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Receiver/CGcmBroadcastReceiver.java
+++ Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Receiver/CGcmBroadcastReceiver.java
@@ -5,10 +5,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
-import android.util.Log;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import fr.univtln.m1dapm.g3.g3vote.Service.CGcmIntentService;
 
@@ -18,16 +14,6 @@ import fr.univtln.m1dapm.g3.g3vote.Service.CGcmIntentService;
 public class CGcmBroadcastReceiver extends WakefulBroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
-    try {
-      JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
-
-      String notificationText = json.getString("alert");
-        Log.i("RECEIVE", "JSON : "+notificationText);
-
-    }catch(JSONException e){
-        Log.i("RECEIVE", "JSONException: " + e.getMessage());
-    }
 
         ComponentName comp = new ComponentName(context.getPackageName(),
                 CGcmIntentService.class.getName());
