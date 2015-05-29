0a14f59bdf40c821f8213833e33a73e5b1b7a158
Kévin Fuster <kevinfuster83@gmail.com>
2015-05-29 10:43:33 +0200

    updates majority judgment's form

--- Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CVoteConfMajorityJugement.java
+++ Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CVoteConfMajorityJugement.java
@@ -1,5 +1,6 @@
 package fr.univtln.m1dapm.g3.g3vote.Interface;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -21,14 +23,24 @@ public class CVoteConfMajorityJugement extends AppCompatActivity {
 
 
     private static ArrayList<CCandidate> mListCandidat = new ArrayList<CCandidate>();
-
+    private String mVoteName;
+    private String mDateDebut;
+    private String mDateFin;
     private CCandidatAdapter mAdapter;
+    private static final String TYPE_VOTE = "MAJORITY";
 
     @Override
     protected void onCreate(Bundle pSavedInstanceState) {
         super.onCreate(pSavedInstanceState);
         setContentView(R.layout.activity_cvote_conf_majority_jugement);
 
+        Bundle extras = getIntent().getExtras();
+        if (extras==null){
+            return;
+        }
+        mVoteName = (String) extras.get("VOTE_NAME");
+        mDateDebut = (String) extras.get("START_DATE");
+        mDateFin = (String) extras.get("END_DATE");
         ListView lList = (ListView)findViewById(R.id.lLVMajorityJugement);
         mListCandidat.add(new CCandidate());
         mListCandidat.add(new CCandidate());
@@ -38,17 +50,19 @@ public class CVoteConfMajorityJugement extends AppCompatActivity {
     }
 
 
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
     public void validateConfMajorityJugement(View pView){
-        for (int i = 0; i <mListCandidat.size() ; i++) {
-            ListView lTest = (ListView)findViewById(R.id.lLVMajorityJugement);
-            View lCde = lTest.getChildAt(i);
-            EditText editText=(EditText) lCde.findViewById(R.id.choiceName);
-            String lString = editText.getText().toString();
-            mListCandidat.get(i).setNomCandidat(lString);
-            Log.i("test", lString);
-        }
+        hideSoftKeyboard(this);
         Intent lIntent = new Intent(this,CTestActivity.class);
         lIntent.putExtra("liste de Candidat", mListCandidat);
+        lIntent.putExtra("VOTE_NAME", mVoteName);
+        lIntent.putExtra("START_DATE", mDateFin);
+        lIntent.putExtra("END_DATE", mDateDebut);
+        lIntent.putExtra("VOTE_TYPE", TYPE_VOTE);
         startActivity(lIntent);
 
     }
