8d11f169118f55a9ec8f1f0a6c8948464a6c721a
Sebi <sebi83140@gmail.com>
2015-05-29 18:17:35 +0200

    Ajoute le debut de l'interface de vote

---
+++ Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CVoteUninominal.java
@@ -0,0 +1,59 @@
+package fr.univtln.m1dapm.g3.g3vote.Interface;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+
+import fr.univtln.m1dapm.g3.g3vote.R;
+
+/**
+ * Created by sebastien on 29/05/15.
+ */
+public class CVoteUninominal extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cvote_uninominal);
+        //Récupération du composant ListView
+        ListView Llist = (ListView)findViewById(R.id.LVChoixCandidatUninominal);
+
+        //TODO:Recuperer les candidats sur le serveur et les afficher
+
+    }
+
+    public void vote(View pView){
+        //TODO:Vérifier qu'un candidat est bien selectionné
+        // On crée le dialogue
+        AlertDialog.Builder lConfirmationDialog = new AlertDialog.Builder(CVoteUninominal.this);
+        // On modifie le titre
+        lConfirmationDialog.setTitle("Confirmation de vote");
+        // On modifie le message
+        lConfirmationDialog.setMessage("Êtes-vous sûr de vouloir voter pour ce candidat ?");
+        // Bouton Oui
+        lConfirmationDialog.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                //TODO:Envoyer le vote au serveur et afficher un toast pour confirmer le vote
+
+                // On termine l'activité et on retourne sur la page principale
+                Intent intent = new Intent(CVoteUninominal.this, CHubActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+        // Bouton non: on ferme le dialogue
+        lConfirmationDialog.setNegativeButton("Non", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        // On affiche le message
+        lConfirmationDialog.show();
+    }
+}
