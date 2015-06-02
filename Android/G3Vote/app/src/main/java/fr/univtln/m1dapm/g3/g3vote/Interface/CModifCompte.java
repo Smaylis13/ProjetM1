7d8cbc8ad0d17a6bb4afa1d00315ea256d99c1ef
pierre <pierre-rouzic@etud.univ-tln.fr>
2015-06-02 14:36:54 +0200

    modification de compte

--- Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CModifCompte.java
+++ Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CModifCompte.java
@@ -1,18 +1,40 @@
 package fr.univtln.m1dapm.g3.g3vote.Interface;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import fr.univtln.m1dapm.g3.g3vote.R;
 
 public class CModifCompte extends ActionBarActivity {
 
+    EditText mModifNom ;
+    EditText mModifPrenom ;
+    EditText mModifMail ;
+    EditText mModifMdp ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cmodif_compte);
+        mModifNom = (EditText)findViewById(R.id.editnommodifcompte);
+        mModifPrenom = (EditText)findViewById(R.id.editprenommodifcompte);
+        mModifMail = (EditText)findViewById(R.id.editmailmodifcompte);
+        mModifMdp = (EditText)findViewById(R.id.editmdpmodifcompte);
+
+
+        mModifNom.setText(CHubActivity.getsLoggedUser().getName());
+        mModifPrenom.setText(CHubActivity.getsLoggedUser().getFirstName());
+        mModifMail.setText(CHubActivity.getsLoggedUser().getEmail());
+        mModifMdp.setText(CHubActivity.getsLoggedUser().getPassword());
+
     }
 
     @Override
@@ -35,5 +57,35 @@ public class CModifCompte extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void validemodifcompte (View view){
+        final Intent lIntent = new Intent(this,CHubActivity.class);
+        // On cree le dialogue
+        AlertDialog.Builder lConfirmationDialog = new AlertDialog.Builder(CModifCompte.this);
+        // On modifie le titre
+        lConfirmationDialog.setTitle("modification de compte");
+        // On modifie le message
+        lConfirmationDialog.setMessage("Voulez-vous vraiment modifier votre compte ?");
+        // Bouton Oui
+        lConfirmationDialog.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(lIntent);
+
+            }
+        });
+
+        // Bouton non: on ferme le dialogue
+        lConfirmationDialog.setNegativeButton("Non", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        // On affiche le message
+        lConfirmationDialog.show();
     }
 }
