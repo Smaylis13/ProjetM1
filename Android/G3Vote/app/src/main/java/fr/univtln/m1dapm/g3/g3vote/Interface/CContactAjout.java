37f573c131f01c13b3b3244eca508a6729eb3196
ISMAIL GACHA <ismail-gacha@etud.univ-tln.fr>
2015-06-07 11:25:08 +0200

    Optimisation ajout de contact

--- Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CContactAjout.java
+++ Android/G3Vote/app/src/main/java/fr/univtln/m1dapm/g3/g3vote/Interface/CContactAjout.java
@@ -1,15 +1,34 @@
 package fr.univtln.m1dapm.g3.g3vote.Interface;
 
+import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.json.JSONArray;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import fr.univtln.m1dapm.g3.g3vote.Communication.CCommunication;
 import fr.univtln.m1dapm.g3.g3vote.Communication.CRequestTypesEnum;
@@ -19,6 +38,9 @@ import fr.univtln.m1dapm.g3.g3vote.R;
 public class CContactAjout extends AppCompatActivity {
 
     private static Context sContext;
+    private SearchView mSearch;
+    private ListView mLvSearch;
+    private List<String> mRes = new ArrayList<String>();
 
     public static Context getsContext() {
         return sContext;
@@ -28,7 +50,75 @@ public class CContactAjout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ccontact_ajout);
-        sContext=getApplicationContext();
+        sContext=this;
+
+
+        mSearch = (SearchView) findViewById(R.id.searchView);
+        mLvSearch = (ListView) findViewById(R.id.lvSearch);
+
+        mLvSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder lConfirmationDialog = new AlertDialog.Builder(CContactAjout.this);
+                lConfirmationDialog.setTitle("Ajouter un contact");
+                lConfirmationDialog.setMessage("Voulez-vous ajouter '"+mLvSearch.getItemAtPosition(position) + "' à vos contacts ?");
+                lConfirmationDialog.setPositiveButton("Ajouter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        CTaskParam lParams=new CTaskParam(CRequestTypesEnum.add_contact,mLvSearch.getItemAtPosition(position));
+                        CCommunication lCom=new CCommunication();
+                        lCom.execute(lParams);
+
+                        CTaskParam lParams2=new CTaskParam(CRequestTypesEnum.get_contacts);
+                        CCommunication lCom2=new CCommunication();
+                        lCom2.execute(lParams2);
+
+                    }
+                });
+                lConfirmationDialog.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                // On affiche le message
+                lConfirmationDialog.show();
+            }
+        });
+
+        //***setOnQueryTextListener***
+        mSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(getBaseContext(), query,
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(newText.length() > 2) {
+                    mLvSearch.setVisibility(View.VISIBLE);
+                    new CallAPI().execute(CCommunication.SERVER_URL+"user/all", newText);
+                }else{
+                    mLvSearch.setVisibility(View.INVISIBLE);
+                }
+                return false;
+            }
+        });
+
+    }
+    public static String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+        InputStream in = entity.getContent();
+        StringBuffer out =
+                new StringBuffer();
+        int n = 1;
+        while (n>0) {
+            byte[] b = new byte[4096];
+            n =  in.read(b);
+            if (n>0) out.append(new String(b, 0, n));
+        }
+        return out.toString();
     }
 
     @Override
@@ -52,38 +142,49 @@ public class CContactAjout extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void ValidationAjoutContact(View view) {
-        final EditText lNomContact = (EditText)findViewById(R.id.editNomContact);
-        final EditText lPrenomContact = (EditText)findViewById(R.id.editPrenomContact);
-        final EditText lMailContact= (EditText)findViewById(R.id.editMailContact);
-        final String lNomContactString = lNomContact.getText().toString();
-        final String lPrenomContactString = lPrenomContact.getText().toString();
-        final String lMailContactString = lMailContact.getText().toString();
+       private class CallAPI extends AsyncTask<String, String, String> {
 
-        if (!(lNomContactString.isEmpty()&&lPrenomContactString.isEmpty()&&lMailContactString.isEmpty())){
+        @Override
+        protected String doInBackground(String... params) {
+            String text="";
 
+            String lService_url = params[0];
+            String pSearch = params[1];
 
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpGet httpGet = new HttpGet(lService_url);
+            try {
+                HttpResponse response = httpClient.execute(httpGet, localContext);
+                HttpEntity entity = response.getEntity();
+                text = getASCIIContentFromEntity(entity);
 
-            /*Intent lIntent = new Intent(this,CTestContactAjout.class);
-            lIntent.putExtra("NomContact",lNomContactString);
-            lIntent.putExtra("PrenomContact",lPrenomContactString);
-            lIntent.putExtra("MailContact",lMailContactString);*/
-            CTaskParam lParams=new CTaskParam(CRequestTypesEnum.add_contact,lMailContactString);
-            CCommunication lCom=new CCommunication();
-            lCom.execute(lParams);
+                JSONArray json = new JSONArray(text);
 
-            CTaskParam lParams2=new CTaskParam(CRequestTypesEnum.get_contacts);
-            CCommunication lCom2=new CCommunication();
-            lCom2.execute(lParams2);
+                mRes.clear();
+                for (int i = 0; i < json.length(); i++){
+                    if (json.getJSONObject(i).getString("email").contains(pSearch)){
+                        mRes.add(json.getJSONObject(i).getString("email"));
+                    }
 
+                }
 
+            } catch (Exception e) {
+                Log.e("SEARCH", e.getLocalizedMessage());
+                return "Error";
 
-
-
-        } else{
-            Toast.makeText(getApplicationContext(), "Entrez tout les champ!", Toast.LENGTH_LONG).show();
+            }
+            return text;
         }
 
-
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s.equals("Error")){
+                  Toast.makeText(sContext, "please try later...", Toast.LENGTH_LONG).show();
+            }else{
+                mLvSearch.setAdapter(new ArrayAdapter<>(sContext,android.R.layout.simple_list_item_1,mRes));
+            }
+        }
     }
 }
